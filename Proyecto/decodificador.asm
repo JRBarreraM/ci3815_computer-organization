a4b36dc6fa254f7131b6e3354ad1c5dc85b982bb
dedchord <15-10123@usb.ve>
2018-11-04 10:50:43 -0400

    Comentarios y optimizacion de registros

--- Proyecto/decodificador.asm
+++ Proyecto/decodificador.asm
@@ -1,5 +1,5 @@
 .data
-test: .word 0x8c000000
+test: .word 0xac242424
 lw_case: .word 35
 sw_case: .word 43
 operation_code: .word 	32,       8,     40,      12,    24,    37,      13,      4,      34,      35,      43,      5,       6,       0
@@ -13,18 +13,31 @@ right_parentesis: .asciiz ")"
 newline: .asciiz "\n"
 
 .text
-main:
+#---------------------------------------------------------------FASE_2---------------------------------------------------------------
+#PLANIFICACION DE REGISTROS FASE 2:
+#		0. $t0 : almacena la instrucción en hexa que se va a traducir e imprimir
+#		1. $t1 : almacena los 6 primeros bits de la instrucción (código de operación)
+#		2. $t2 : almacena el índice del arreglo de tipos de operación correspondiente a la operación por la
+#			 que se pasea la búsqueda en un momento dado (iterador de operation_type)
+#		3. $t3 : almacena el código de operación correspondiente a la operación por la que se pasea la búsqueda
+#			 en un momento dado, y luego de encontrada la operación, se usa como temporal para información que
+#			 se extraiga de la operación para imprimirla (rd,rs,rt u offset)
+#		4. $t4 : almacena el índice del arreglo de códigos de operación correspondiente a la operación por la
+#			 que se pasea la búsqueda en un momento dado (iterador de operation_code)
+#		5. $t5 : almacena el índice del arreglo de nombres de operación correspondiente a la operación por la
+#			 que se pasea la búsqueda en un momento dado (iterador de operation_name)
+#		7. $t6 : almacena el .asciiz "I"
+#
 
+main:
 	# Inicializamos $t2 en 0, iterador de operation_type
 	addi $t2,$zero,0
 	# Inicializamos $t4 en 0, iterador de operation_code
 	addi $t4,$zero,0
 	# Inicializamos $t5 en 0, iterador de operation_name
 	addi $t5,$zero,0
-	# Almacenamos en $t7 el ascii I	
-	la $t7,I
-	lw $t8,lw_case
-	lw $t9,sw_case
+	# Almacenamos en $t6 el ascii I	
+	la $t6,I
 			
 	#Extraemos el código de operación
 	lw $t0,test			
@@ -74,91 +87,100 @@ main:
 
 #Hay operaciones que no siguen el formato R o I exactamente, por lo que son casos especiales
 	
-	beq $t3,0,exit		#la instreccion es halt
-	beq $t3,$t8,lw_sw_case	#la instruccion es lw o sw
-	beq $t3,$t9,lw_sw_case
+	beq $t3,0,exit		#la instrucción es halt
+	beq $t3,35,lw_sw_case	#la instrucción es lw o sw
+	beq $t3,43,lw_sw_case
+	la $t3,operation_type($t2)#verificamos si la operacion es de tipo I
+	beq $t3,$t6,typeI	#sino es de tipo R
+
+	#Imprimimos en formato R
 	
-	#Imprimimos los registros involucrados
-	#Extraemos el rs
-	lw $t0,test
-	andi $t6,$t0,0x03e00000	#apagamos los bits que no corresponde al rs
-	srl $t6, $t6, 21	#rodamos el rs al inicio
-	#Imprimimos el signo de dólar
-	li $v0,4
-	la $a0,dollar
-	syscall	
-	#Imprimimos el rs
-	li $v0,1
-	move $a0,$t6
-	syscall
-	#Imprimimos un espacio en blanco
-	li $v0,4
-	la $a0,space
-	syscall
-	#Extraemos el rt
-	lw $t0,test
-	andi $t6,$t0,0x001f0000 #apagamos los bits que no corresponde al rt
-	srl $t6, $t6, 16	#rodamos el rt al inicio
-	#Imprimimos el signo de dólar
-	li $v0,4
-	la $a0,dollar
-	syscall
-	#Imprimimos el rt
-	li $v0,1
-	move $a0,$t6
-	syscall
-	#Imprimimos un espacio en blanco
-	li $v0,4
-	la $a0,space
-	syscall
-	
-	la $t6,operation_type($t2)#si la operacion es de tipo I, decodificamos
-	beq $t7,$t6,typeI	#la direccion de de 16 bits (offset), sino es de tipo R
-				#y decodificamos el registro faltante (rd)
 	#Extraemos el rd
 	lw $t0,test
-	andi $t6,$t0,0x0000f800 #apagamos los bits que no corresponde al rd
-	srl $t6, $t6, 11	#rodamos el rd al inicio
+	andi $t3,$t0,0x0000f800 #apagamos los bits que no corresponde al rd
+	srl $t3, $t3, 11	#rodamos el rd al inicio
 	#Imprimimos el signo de dólar
 	li $v0,4
 	la $a0,dollar
 	syscall
 	#Imprimimos el rd
 	li $v0,1
-	move $a0,$t6
+	move $a0,$t3
 	syscall
-	
-	#Imprimimos una nueva linea
+
+	#Imprimimos un espacio en blanco
 	li $v0,4
-	la $a0,newline
+	la $a0,space
 	syscall
 	
-	typeI:
-	#Extraemos el offset
+	#Extraemos el rs
 	lw $t0,test
-	andi $t6,$t0,0x0000ffff #apagamos los bits que no corresponde al offset
-	#Imprimimos el offset	#ya esta al inicio
-	li $v0,1
-	move $a0,$t6
-	syscall	
-	
-	#Imprimimos una nueva linea
+	andi $t3,$t0,0x03e00000	#apagamos los bits que no corresponde al rs
+	srl $t3, $t3, 21	#rodamos el rs al inicio
+	#Imprimimos el signo de dólar
 	li $v0,4
-	la $a0,newline
+	la $a0,dollar
+	syscall	
+	#Imprimimos el rs
+	li $v0,1
+	move $a0,$t3
 	syscall
-	
-	lw_sw_case:
+
+	#Imprimimos un espacio en blanco
+	li $v0,4
+	la $a0,space
+	syscall
+
 	#Extraemos el rt
 	lw $t0,test
-	andi $t6,$t0,0x001f0000 #apagamos los bits que no corresponde al rt
-	srl $t6, $t6, 16	#rodamos el rt al inicio
+	andi $t3,$t0,0x001f0000 #apagamos los bits que no corresponde al rt
+	srl $t3, $t3, 16	#rodamos el rt al inicio
 	#Imprimimos el signo de dólar
 	li $v0,4
 	la $a0,dollar
 	syscall
 	#Imprimimos el rt
 	li $v0,1
-	move $a0,$t6
+	move $a0,$t3
+	syscall
+
+	#Imprimimos una nueva linea
+	li $v0,4
+	la $a0,newline
+	syscall
+	j exit	#terminamos con la operación
+
+	#Imprimimos en formato I
+	typeI:
+
+	#Extraemos el rt
+	lw $t0,test
+	andi $t3,$t0,0x001f0000 #apagamos los bits que no corresponde al rt
+	srl $t3, $t3, 16	#rodamos el rt al inicio
+	#Imprimimos el signo de dólar
+	li $v0,4
+	la $a0,dollar
+	syscall
+	#Imprimimos el rt
+	li $v0,1
+	move $a0,$t3
+	syscall
+	#Imprimimos un espacio en blanco
+	li $v0,4
+	la $a0,space
+	syscall
+
+	#Extraemos el rs
+	lw $t0,test
+	andi $t3,$t0,0x03e00000	#apagamos los bits que no corresponde al rs
+	srl $t3, $t3, 21	#rodamos el rs al inicio
+	#Imprimimos el signo de dólar
+	li $v0,4
+	la $a0,dollar
+	syscall	
+	#Imprimimos el rs
+	li $v0,1
+	move $a0,$t3
 	syscall
 	#Imprimimos un espacio en blanco
 	li $v0,4
@@ -167,10 +189,42 @@ main:
 
 	#Extraemos el offset
 	lw $t0,test
-	andi $t6,$t0,0x0000ffff #apagamos los bits que no corresponde al offset
+	andi $t3,$t0,0x0000ffff #apagamos los bits que no corresponde al offset
 	#Imprimimos el offset	#ya esta al inicio
 	li $v0,1
-	move $a0,$t6
+	move $a0,$t3
+	syscall	
+	
+	#Imprimimos una nueva linea
+	li $v0,4
+	la $a0,newline
+	syscall
+	j exit	#terminamos con la operación
+	
+	lw_sw_case:
+	#Extraemos el rt
+	lw $t0,test
+	andi $t3,$t0,0x001f0000 #apagamos los bits que no corresponde al rt
+	srl $t3, $t3, 16	#rodamos el rt al inicio
+	#Imprimimos el signo de dólar
+	li $v0,4
+	la $a0,dollar
+	syscall
+	#Imprimimos el rt
+	li $v0,1
+	move $a0,$t3
+	syscall
+	#Imprimimos un espacio en blanco
+	li $v0,4
+	la $a0,space
+	syscall
+
+	#Extraemos el offset
+	lw $t0,test
+	andi $t3,$t0,0x0000ffff #apagamos los bits que no corresponde al offset
+	#Imprimimos el offset	#ya esta al inicio
+	li $v0,1
+	move $a0,$t3
 	syscall
 
 	#Imprimimos un paréntesis izquierdo
@@ -184,11 +238,11 @@ main:
 	
 	#Extraemos el rs
 	lw $t0,test
-	andi $t6,$t0,0x03e00000	#apagamos los bits que no corresponde al rs
-	srl $t6, $t6, 21	#rodamos el rs al inicio
+	andi $t3,$t0,0x03e00000	#apagamos los bits que no corresponde al rs
+	srl $t3, $t3, 21	#rodamos el rs al inicio
 	#Imprimimos el rs
 	li $v0,1
-	move $a0,$t6
+	move $a0,$t3
 	syscall
 
 	#Imprimimos un paréntesis derecho
@@ -200,6 +254,7 @@ main:
 	li $v0,4
 	la $a0,newline
 	syscall
+	j exit	#terminamos con la operación
 	
 	exit:
 	#no hacemos nada mas,terminariomos el programa,pero eso corresponde al main
